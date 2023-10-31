627f7b3fce72661327ec297921b996c39ceed1a1
Robin Assam <vanasam09@gmail.com>
2023-10-31 15:53:36 +0100

    Player class

--- src/Phase2/Menu.java
+++ src/Phase2/Menu.java
@@ -1,5 +1,7 @@
 package Phase2;
 
+
+
 public class Menu {
-    
+
 }
--- src/Phase2/Player.java
+++ src/Phase2/Player.java
@@ -1,5 +1,29 @@
 package Phase2;
 
 public class Player {
-    
+
+    //comment
+    private String name;
+    private int currentScore;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCurrentScore() {
+        return currentScore;
+    }
+
+    public void setCurrentScore(int currentScore) {
+        this.currentScore = currentScore;
+    }
+
+    public Player(String name, int currentScore) {
+        this.name = name;
+        this.currentScore = currentScore;
+    }
 }
