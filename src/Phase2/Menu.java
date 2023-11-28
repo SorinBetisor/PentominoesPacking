48a865f1a861dffbc83e71e28e3b71da8a7cd673
Robin Assam <vanasam09@gmail.com>
2023-11-28 14:36:36 +0100

    Added Controls Added a sound icon (needs to change) Fixed music problem

--- src/Phase2/Menu.java
+++ src/Phase2/Menu.java
@@ -1,4 +1,6 @@
 package Phase2;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 import javax.imageio.ImageIO;
 import javax.sound.sampled.LineUnavailableException;
@@ -13,7 +15,7 @@ public class Menu {
 
     //To Add : change sound icon, use sound sliders, add title, add name text box (back).
     private Player player;
-    private JFrame frame = new JFrame("PENTRIS Menu");
+    private static JFrame frame = new JFrame("PENTRIS Menu");
     private JTextField inputField;
     private SoundPlayerUsingClip soundPlayer;
     private JCheckBox musicToggle;
@@ -45,7 +47,7 @@ public class Menu {
         return titleLabel;
     }
 
-   
+
     public Menu() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
@@ -71,16 +73,19 @@ public class Menu {
         gbc.insets = new Insets(20, 0, 20, 0);
         titleLabel.setBackground(randomColor);
         backgroundPanel.add(titleLabel, gbc);
-        
+
 
         backgroundPanel.add(createButtonPanel(), gbc);
 
 
-        musicToggle = new JCheckBox("Toggle Music");
+        musicToggle = new JCheckBox("\uD83D\uDD0A"); //need to change
         musicToggle.setSelected(true);
+        musicToggle.setOpaque(false);
+        musicToggle.setContentAreaFilled(false);
         musicToggle.addActionListener(e -> toggleMusic());
-        gbc.insets = new Insets(10, 0, 0, 0); // spacing 4 music toggle
+        gbc.insets = new Insets(10, 0, 0, 0);
         backgroundPanel.add(musicToggle, gbc);
+
 
         frame.setVisible(true);
 
@@ -98,30 +103,78 @@ public class Menu {
         buttonPanel.setLayout(new GridLayout(3, 1, 10, 10));
         buttonPanel.setOpaque(false);
 
-        JButton randomOrderButton = createButton("Random Order");
-        JButton bestOrderButton = createButton("Best Order");
+        JButton randomOrderButton = createButton("PLAY");
+        JButton bestOrderButton = createButton("Bot - Best Order");
         JButton botButton = createButton("Bot");
+
+        JButton controlsButton = new JButton("Controls");
+        controlsButton.setFont(new Font("Arial", Font.BOLD, 14));
+
 
         customizeButton(randomOrderButton, Color.pink, Color.pink);
         customizeButton(bestOrderButton, Color.GREEN, Color.green);
         customizeButton(botButton, Color.white, Color.white);
+        customizeButton(controlsButton,Color.ORANGE,Color.ORANGE);
 
         buttonPanel.add(randomOrderButton);
         buttonPanel.add(bestOrderButton);
         buttonPanel.add(botButton);
+        buttonPanel.add(controlsButton);
+
 
         randomOrderButton.addActionListener(e -> {
-            System.out.println("Random Order Button Clicked");
-            toggleMusic();
+            //System.out.println("Random Order Button Clicked");
+            //toggleMusic();
             frame.dispose();
             Tetris tetris = new Tetris();
             tetris.runTetris();
             savePlayerInfo();
         });
 
+        controlsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showControlsDialog();
+            }
+        });
+
         //4 commit
         return buttonPanel;
     }
+
+    public void showControlsDialog(){
+
+        JDialog controlsDialog = new JDialog(frame, "Game Controls", true);
+        controlsDialog.setSize(400, 300);
+        controlsDialog.setLocationRelativeTo(frame);
+
+
+        BackgroundPanel controlsBackground = new BackgroundPanel("src/Phase2/misc/photo.jpg");
+        controlsBackground.setLayout(new BorderLayout());
+        controlsDialog.setContentPane(controlsBackground);
+
+
+        String controlsText = "<html><body style='text-align: center;'>"
+                + "<h3>Controls:</h3>"
+                + "SPACE - Drops the piece instantly<br>"
+                + "LEFT ARROW - Move piece left<br>"
+                + "RIGHT ARROW - Move piece right<br>"
+                + "UP ARROW - Decrease speed<br>"
+                + "DOWN ARROW - Increase speed<br>"
+                + "D - Rotate piece clockwise<br>"
+                + "A - Rotate piece anticlockwise"
+                + "</body></html>";
+
+
+        JLabel controlsLabel = new JLabel(controlsText, SwingConstants.CENTER);
+        controlsLabel.setForeground(Color.WHITE);
+        controlsBackground.add(controlsLabel);
+
+        // Display the dialog
+        controlsDialog.setVisible(true);
+    }
+
+
 
     private void customizeButton(JButton button, Color foregroundColor, Color borderColor) {
         button.setBackground(new Color(59, 89, 182));
@@ -131,8 +184,8 @@ public class Menu {
         button.setBorderPainted(true);
         button.setBorder(BorderFactory.createLineBorder(borderColor, 4));
         button.setPreferredSize(new Dimension(200, 50));
-        
-    
+
+
     }
 
     private JButton createButton(String text) {
