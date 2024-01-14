95ba754eb7b8d5bf71da8edf6212ceadda5fe000
Robin Assam <vanasam09@gmail.com>
2024-01-14 15:45:38 +0100

    menu scenebuilder

--- src/Phase3/Visualizer/Controller.java
+++ src/Phase3/Visualizer/Controller.java
@@ -1,22 +1,31 @@
 package Phase3.Visualizer;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 
 import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
 
     @FXML
     private Label label1;
-
     @FXML
     private Label label2;
-
     @FXML
-    private ChoiceBox choiceBox1;
-
+    private ChoiceBox <String> pieces;
     @FXML
-    private ChoiceBox choiceBox2;
-    
+    private ChoiceBox<String> algorithm;
+
+    private String[] piecesList = {"Parcels", "Pentomino"};
+    private String[] algorithms = {"Dancing Links"};
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        pieces.getItems().addAll(piecesList);
+        algorithm.getItems().addAll(algorithms);
+    }
 }
